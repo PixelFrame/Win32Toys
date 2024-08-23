3247eed01ddebc56ea1494fcc79fefe8973ebd6e
Pixel Frame <pm421@live.com>
2024-08-23 13:09:16 +0800

    Packet buffering and console print optimization

---
+++ SocketSniffer/Packet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SocketSniffer
+{
+    internal class Packet
+    {
+        public DateTime Time { get; }
+        public byte[] Data { get; }
+
+        public Packet(byte[] Buffer, int Length) 
+        {
+            Time = HRTime.Now;
+            Data = new byte[Length];
+            Array.Copy(Buffer, Data, Length);
+        }
+    }
+}
--- SocketSniffer/Program.cs
+++ SocketSniffer/Program.cs
@@ -1,22 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Threading;
+using System.Collections.Concurrent;
 
 namespace SocketSniffer
 {
     internal class Program
     {
         private static Socket MainSocket = null;
+        private static SocketError Error = SocketError.Success;
         private static readonly byte[] ByteData = new byte[9000];
         private static bool ContinueCapturing = true;
+        private static bool UnableToContinueCapturing = false;
+        private static int PacketCounter = 0;
+        private static int FailedPacketCounter = 0;
+        private static readonly ConcurrentQueue<Packet> PacketBuffer = new();
 
         private static IPAddress BindAddr;
         private static StreamWriter OutputFileStreamWriter;
+        private static Task WriteTask;
+        private static Timer PrintTimer;
 
         private const string USAGE_MSG = "SocketSniffer.exe <BindingAddress> <OutputFile>";
 
@@ -27,7 +35,7 @@ namespace SocketSniffer
                 Console.WriteLine(USAGE_MSG);
                 return;
             }
-            if(!IPAddress.TryParse(args[0], out BindAddr))
+            if (!IPAddress.TryParse(args[0], out BindAddr))
             {
                 Console.WriteLine("Invalid address");
                 Console.WriteLine(USAGE_MSG);
@@ -36,25 +44,39 @@ namespace SocketSniffer
 
             OutputFileStreamWriter = new StreamWriter(args[1]);
 
-            Init();
+            InitSocket();
+            PrintTimer = new(PrintCounter, null, 1000, 1000);
+            WriteTask = Task.Run(WriteDump);
+            Console.WriteLine("Press 'X' to quit");
+
             while (true)
             {
-                Console.WriteLine("Press 'X' to quit");
-                var cki = Console.ReadKey(true);
-
-                // Exit if the user pressed the 'X' key.
-                if (cki.Key == ConsoleKey.X) break;
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    if (key.Key == ConsoleKey.X) break;
+                }
+                if (UnableToContinueCapturing)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Fatal error occurred, exiting...");
+                }
+                Thread.Sleep(100);
             }
             ContinueCapturing = false;
+            Console.WriteLine();
+            Console.WriteLine("Waiting for all packets written to file...");
+            WriteTask.Wait();
             MainSocket.Close();
             OutputFileStreamWriter.WriteLine();
             OutputFileStreamWriter.Close();
 
+            PrintTimer.Dispose();
             MainSocket.Dispose();
             OutputFileStreamWriter.Dispose();
         }
 
-        private static void Init()
+        private static void InitSocket()
         {
             MainSocket = new Socket(AddressFamily.InterNetwork,
                                             SocketType.Raw, ProtocolType.IP);
@@ -70,7 +92,7 @@ namespace SocketSniffer
             MainSocket.IOControl(IOControlCode.ReceiveAll, byTrue, byOut);
             // Start receiving the packets asynchronously.
             MainSocket.BeginReceive(ByteData, 0, ByteData.Length,
-                                    SocketFlags.None,
+                                    SocketFlags.None, out Error,
                                     new AsyncCallback(OnReceive), null);
         }
 
@@ -79,7 +101,26 @@ namespace SocketSniffer
             try
             {
                 int nReceived = MainSocket.EndReceive(asyncResult);
-                WriteDump(nReceived);
+                PacketCounter++;
+                PacketBuffer.Enqueue(new(ByteData, nReceived));
+            }
+            catch (ObjectDisposedException) { }
+            catch (SocketException)
+            {
+                // Somehow there can be UDP packets larger than 9000 bytes, not sure if related to URO🧐
+                if (Error == SocketError.NoBufferSpaceAvailable)
+                {
+                    FailedPacketCounter++;
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine();
+                Console.WriteLine(exception.ToString());
+                UnableToContinueCapturing = true;
+            }
+            finally
+            {
                 if (ContinueCapturing)
                 {
                     MainSocket.BeginReceive(ByteData, 0, ByteData.Length,
@@ -87,22 +128,32 @@ namespace SocketSniffer
                                             new AsyncCallback(OnReceive), null);
                 }
             }
-            catch (ObjectDisposedException) { }
-            catch (Exception exception)
+        }
+
+        private static void WriteDump()
+        {
+            while (ContinueCapturing || PacketBuffer.Count != 0)
             {
-                Console.WriteLine(exception.ToString());
+                if (PacketBuffer.TryDequeue(out var pkt))
+                {
+                    var sb = new StringBuilder();
+                    sb.Append("> ")
+                        .Append(pkt.Time.ToString("yyyy-MM-dd HH:mm:ss.fffffff"))
+                        .Append(' ')
+                        .Append(pkt.Data.Select(b => b.ToString("X2")).Aggregate((a, b) => a + b));
+                    OutputFileStreamWriter.WriteLine(sb.ToString());
+                }
+                else
+                {
+                    Thread.Sleep(50);
+                }
             }
         }
 
-        private static void WriteDump(int recvLen)
+        private static void PrintCounter(object state)
         {
-            Console.WriteLine("Writing dump file");
-            var sb = new StringBuilder();
-            sb.Append("> ")
-                .Append(HRTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffffff"))
-                .Append(' ')
-                .Append(ByteData.Take(recvLen).Select(b => b.ToString("X2")).Aggregate((a, b) => a + b));
-            OutputFileStreamWriter.WriteLine(sb.ToString());
+            Console.Write("\rCaptured {0} packets, saved {1} packets, pending {2} packets, unable to save {3} packets                             ",
+                PacketCounter, PacketCounter - PacketBuffer.Count, PacketBuffer.Count, FailedPacketCounter);
         }
     }
 }
