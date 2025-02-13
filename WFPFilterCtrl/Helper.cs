bc6d6967972bd94bfd70f18cad75deadbc1c6acd
Pixel Frame <pm421@live.com>
2025-02-13 15:23:38 +0800

    Add WFPFilterCtrl

---
+++ WFPFilterCtrl/Helper.cs
@@ -0,0 +1,219 @@
+﻿using BidirectionalDict;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.NetworkManagement.WindowsFilteringPlatform;
+using Windows.Win32.Security;
+
+namespace WFPFilterCtrl
+{
+    internal class Helper
+    {
+        public static BiDictionary<Guid, string> ConditionGuidTable = [];
+        public static BiDictionary<Guid, string> LayerGuidTable = [];
+        public static BiDictionary<Guid, string> SubLayerGuidTable = [];
+        public static BiDictionary<Guid, string> CalloutGuidTable = [];
+
+        public static void GenerateTables()
+        {
+            GenerateConditionGuidTable();
+            GenerateLayerGuidTable();
+            GenerateSubLayerGuidTable();
+            GenerateCalloutGuidTable();
+        }
+
+        private static void GenerateConditionGuidTable()
+        {
+            typeof(Windows.Win32.PInvoke).GetFields(System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static).ToList().ForEach(f =>
+            {
+                if (f.Name.StartsWith("FWPM_CONDITION_") && f.FieldType == typeof(Guid))
+                {
+                    ConditionGuidTable.AddOrUpdate((Guid)f.GetValue(null), f.Name);
+                }
+            });
+
+            // Added since Cu
+            ConditionGuidTable.AddOrUpdate(Guid.Parse("81BC78FB-F28D-4886-A604-6ACC261F261B") , "FWPM_CONDITION_ALE_PACKAGE_FAMILY_NAME");
+        }
+
+        public static string TranslateConditionGuid(Guid guid)
+        {
+            return ConditionGuidTable.Contains(guid) ? ConditionGuidTable[guid].Substring(15) : guid.ToString();
+        }
+
+        public static Guid GetConditionGuid(string name)
+        {
+            return ConditionGuidTable.Contains(name) ? ConditionGuidTable[name] : Guid.Parse(name);
+        }
+
+        private static void GenerateLayerGuidTable()
+        {
+            typeof(Windows.Win32.PInvoke).GetFields(System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static).ToList().ForEach(f =>
+            {
+                if (f.Name.StartsWith("FWPM_LAYER_") && f.FieldType == typeof(Guid))
+                {
+                    LayerGuidTable.AddOrUpdate((Guid)f.GetValue(null), f.Name);
+                }
+            });
+        }
+
+        public static string TranslateLayerGuid(Guid guid)
+        {
+            return LayerGuidTable.Contains(guid) ? LayerGuidTable[guid] : guid.ToString();
+        }
+
+        public static Guid GetLayerGuid(string name)
+        {
+            return LayerGuidTable.Contains(name) ? LayerGuidTable[name] : Guid.Parse(name);
+        }
+
+        private static void GenerateSubLayerGuidTable()
+        {
+            typeof(Windows.Win32.PInvoke).GetFields(System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static).ToList().ForEach(f =>
+            {
+                if (f.Name.StartsWith("FWPM_SUBLAYER_") && f.FieldType == typeof(Guid))
+                {
+                    SubLayerGuidTable.AddOrUpdate((Guid)f.GetValue(null), f.Name);
+                }
+            });
+        }
+
+        public static string TranslateSubLayerGuid(Guid guid)
+        {
+            return SubLayerGuidTable.Contains(guid) ? SubLayerGuidTable[guid] : guid.ToString();
+        }
+
+        private static void GenerateCalloutGuidTable()
+        {
+            typeof(Windows.Win32.PInvoke).GetFields(System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static).ToList().ForEach(f =>
+            {
+                if (f.Name.StartsWith("FWPM_CALLOUT_") && f.FieldType == typeof(Guid))
+                {
+                    CalloutGuidTable.AddOrUpdate((Guid)f.GetValue(null), f.Name);
+                }
+            });
+        }
+
+        public static string TranslateCalloutGuid(Guid guid)
+        {
+            return CalloutGuidTable.Contains(guid) ? CalloutGuidTable[guid] : guid.ToString();
+        }
+
+        public unsafe static string TranslateValue(FWP_VALUE0 value)
+        {
+            GCHandle handle = GCHandle.Alloc(value, GCHandleType.Pinned);
+            FWP_CONDITION_VALUE0* pValue = (FWP_CONDITION_VALUE0*)handle.AddrOfPinnedObject();
+            var res = TranslateValue(*pValue);
+            handle.Free();
+            return res;
+        }
+
+        public unsafe static string TranslateValue(FWP_CONDITION_VALUE0 value)
+        {
+            switch (value.type)
+            {
+                case FWP_DATA_TYPE.FWP_EMPTY:
+                    return "Empty";
+                case FWP_DATA_TYPE.FWP_UINT8:
+                    return (value.Anonymous.uint8).ToString();
+                case FWP_DATA_TYPE.FWP_UINT16:
+                    return (value.Anonymous.uint16).ToString();
+                case FWP_DATA_TYPE.FWP_UINT32:
+                    return (value.Anonymous.uint32).ToString();
+                case FWP_DATA_TYPE.FWP_UINT64:
+                    return (*value.Anonymous.uint64).ToString();
+                case FWP_DATA_TYPE.FWP_INT8:
+                    return (value.Anonymous.int8).ToString();
+                case FWP_DATA_TYPE.FWP_INT16:
+                    return (value.Anonymous.int16).ToString();
+                case FWP_DATA_TYPE.FWP_INT32:
+                    return (value.Anonymous.int32).ToString();
+                case FWP_DATA_TYPE.FWP_INT64:
+                    return (*value.Anonymous.int64).ToString();
+                case FWP_DATA_TYPE.FWP_FLOAT:
+                    return (value.Anonymous.float32).ToString();
+                case FWP_DATA_TYPE.FWP_DOUBLE:
+                    return (*value.Anonymous.double64).ToString();
+                case FWP_DATA_TYPE.FWP_BYTE_ARRAY16_TYPE:
+                    {
+                        var arr = (*value.Anonymous.byteArray16).byteArray16;
+                        return $"{arr[0]:X2}{arr[1]:X2}{arr[2]:X2}{arr[3]:X2}{arr[4]:X2}{arr[5]:X2}{arr[6]:X2}{arr[7]:X2}{arr[8]:X2}{arr[9]:X2}{arr[10]:X2}{arr[11]:X2}{arr[12]:X2}{arr[13]:X2}{arr[14]:X2}{arr[15]:X2}";
+                    }
+                case FWP_DATA_TYPE.FWP_BYTE_BLOB_TYPE:
+                    {
+                        var blob = *value.Anonymous.byteBlob;
+                        return Encoding.Unicode.GetString(blob.data, (int)blob.size);
+                    }
+                case FWP_DATA_TYPE.FWP_SID:
+                    {
+                        PWSTR strSid;
+                        PSID pSID = new(value.Anonymous.sid);
+                        PInvoke.ConvertSidToStringSid(pSID, &strSid);
+                        var res = strSid.ToString();
+                        PInvoke.LocalFree(new HLOCAL(strSid));
+                        return res;
+                    }
+                case FWP_DATA_TYPE.FWP_SECURITY_DESCRIPTOR_TYPE:
+                    {
+                        var sd = (*value.Anonymous.sd).data;
+                        PSECURITY_DESCRIPTOR pSD = new(sd);
+                        PWSTR strSD;
+                        uint strSDLen;
+                        PInvoke.ConvertSecurityDescriptorToStringSecurityDescriptor(pSD,
+                            PInvoke.SDDL_REVISION_1,
+                            OBJECT_SECURITY_INFORMATION.DACL_SECURITY_INFORMATION /*| OBJECT_SECURITY_INFORMATION.OWNER_SECURITY_INFORMATION | OBJECT_SECURITY_INFORMATION.GROUP_SECURITY_INFORMATION | OBJECT_SECURITY_INFORMATION.SACL_SECURITY_INFORMATION*/, // Only DACL matters
+                            &strSD, &strSDLen);
+                        var res = strSD.ToString();
+                        PInvoke.LocalFree(new HLOCAL(strSD));
+                        return res;
+                    }
+                case FWP_DATA_TYPE.FWP_TOKEN_INFORMATION_TYPE:
+                    {
+                        var tokenInfo = *value.Anonymous.tokenInformation;
+                        return "Not implemented: FWP_TOKEN_INFORMATION_TYPE";
+                    }
+                case FWP_DATA_TYPE.FWP_TOKEN_ACCESS_INFORMATION_TYPE:
+                    {
+                        var tokenAccessInfo = *value.Anonymous.tokenAccessInformation;
+                        return "Not implemented: FWP_TOKEN_ACCESS_INFORMATION_TYPE";
+                    }
+                case FWP_DATA_TYPE.FWP_UNICODE_STRING_TYPE:
+                    return value.Anonymous.unicodeString.ToString();
+                case FWP_DATA_TYPE.FWP_BYTE_ARRAY6_TYPE:
+                    {
+                        var arr = (*value.Anonymous.byteArray6).byteArray6;
+                        return $"{arr[0]:X2}{arr[1]:X2}{arr[2]:X2}{arr[3]:X2}{arr[4]:X2}{arr[5]:X2}";
+                    }
+                case FWP_DATA_TYPE.FWP_V4_ADDR_MASK:
+                    {
+                        var pValue = (FWP_V4_ADDR_AND_MASK*)value.Anonymous.uint64;
+                        var addr = new IPAddress(pValue->addr);
+                        var mask = new IPAddress(pValue->mask);
+                        return $"{addr}/{mask}";
+                    }
+                case FWP_DATA_TYPE.FWP_V6_ADDR_MASK:
+                    {
+                        var pValue = (FWP_V6_ADDR_AND_MASK*)value.Anonymous.uint64;
+                        var addrbytes = (new Span<byte>(pValue->addr.Value, 16)).ToArray();
+                        var addr = new IPAddress(addrbytes);
+                        var maskLen = pValue->prefixLength;
+                        return $"{addr}/{maskLen}";
+                    }
+                case FWP_DATA_TYPE.FWP_RANGE_TYPE:
+                    {
+                        var pValue = (FWP_RANGE0*)value.Anonymous.uint64;
+                        return $"{TranslateValue(pValue->valueLow)}-{TranslateValue(pValue->valueHigh)}";
+                    }
+                default:
+                    return "Unknown value";
+            }
+        }
+
+    }
+}
---
+++ WFPFilterCtrl/Program.cs
@@ -0,0 +1,276 @@
+﻿using Microsoft.Win32.SafeHandles;
+using Microsoft.Windows.SDK.Win32Docs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.NetworkManagement.WindowsFilteringPlatform;
+using Windows.Win32.Security;
+
+namespace WFPFilterCtrl
+{
+    internal class Program
+    {
+        const string USAGE = """
+            WFPFilterCtrl.exe list [-v]
+            WFPFilterCtrl.exe query <filterId>
+            WFPFilterCtrl.exe remove <filterId>
+            WFPFilterCtrl.exe add -n <name> [-d <description>] -l <layer> -a <action> [-w <weight>] [-c <field> <match> <value>]
+                              action only supports 'block' or 'permit'
+                              multiple conditions can be added with -c
+                              layer and field can be GUID or symbolic name
+            """;
+
+        static unsafe void Main(string[] args)
+        {
+            uint dwRet;
+            HANDLE engineHandle;
+
+            Helper.GenerateTables();
+
+            dwRet = PInvoke.FwpmEngineOpen0(null, PInvoke.RPC_C_AUTHN_WINNT, null, null, &engineHandle);
+            if (dwRet != 0)
+            {
+                Console.WriteLine($"FwpmEngineOpen0 failed with error code: 0x{dwRet:X8}");
+                return;
+            }
+
+            ParseArgs(args, engineHandle);
+
+            dwRet = PInvoke.FwpmEngineClose0(engineHandle);
+            if (dwRet != 0)
+            {
+                Console.WriteLine($"FwpmEngineClose0 failed with error code: 0x{dwRet:X8}");
+                return;
+            }
+        }
+
+        static void ParseArgs(string[] args, HANDLE engineHandle)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine(USAGE);
+                return;
+            }
+            switch (args[0])
+            {
+                case "list":
+                    if (args.Length == 2 && args[1] == "-v")
+                    {
+                        PrintFilters(engineHandle, true);
+                    }
+                    else
+                    {
+                        PrintFilters(engineHandle);
+                    }
+                    break;
+                case "query":
+                    if (args.Length != 2)
+                    {
+                        Console.WriteLine(USAGE);
+                        return;
+                    }
+                    QueryFilterById(engineHandle, ulong.Parse(args[1]));
+                    break;
+                case "remove":
+                    if (args.Length != 2)
+                    {
+                        Console.WriteLine(USAGE);
+                        return;
+                    }
+                    RemoveFilterById(engineHandle, ulong.Parse(args[1]));
+                    break;
+                case "add":
+                    ProcessAddArgs(args, engineHandle);
+                    break;
+                default:
+                    Console.WriteLine(USAGE);
+                    break;
+            }
+        }
+
+        static void ProcessAddArgs(string[] args, HANDLE engineHandle)
+        {
+            var name = "";
+            var description = "";
+            var layer = "";
+            var action = "";
+            var weight = 0ul;
+            var conditions = new List<(string, string, string)>();
+            for (int i = 1; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "-n":
+                        name = args[++i];
+                        break;
+                    case "-d":
+                        description = args[++i];
+                        break;
+                    case "-l":
+                        layer = args[++i];
+                        break;
+                    case "-a":
+                        action = args[++i];
+                        break;
+                    case "-w":
+                        weight = ulong.Parse(args[++i]);
+                        break;
+                    case "-c":
+                        conditions.Add((args[++i], args[++i], args[++i]));
+                        break;
+                    default:
+                        Console.WriteLine(USAGE);
+                        return;
+                }
+            }
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(layer) || string.IsNullOrEmpty(action))
+            {
+                Console.WriteLine(USAGE);
+                return;
+            }
+            AddFilter(engineHandle, name, description, layer, action, conditions, weight);
+        }
+
+        static unsafe void PrintFilters(HANDLE engineHandle, bool verbose = false)
+        {
+            uint dwRet;
+            HANDLE enumHandle;
+            FWPM_FILTER0** filters = null;
+            uint numEntriesReturned;
+
+            dwRet = PInvoke.FwpmFilterCreateEnumHandle0(engineHandle, null, &enumHandle);
+            if (dwRet != 0)
+            {
+                Console.WriteLine($"FwpmFilterCreateEnumHandle0 failed with error code: 0x{dwRet:X8}");
+                return;
+            }
+
+            dwRet = PInvoke.FwpmFilterEnum0(engineHandle, enumHandle, uint.MaxValue, &filters, &numEntriesReturned);
+            if (dwRet != 0)
+            {
+                PInvoke.FwpmFilterDestroyEnumHandle0(engineHandle, enumHandle);
+                Console.WriteLine($"FwpmFilterEnum0 failed with error code: 0x{dwRet:X8}");
+                return;
+            }
+
+            for (uint i = 0; i < numEntriesReturned; i++)
+            {
+                if (verbose)
+                {
+                    PrintFilterVerbose(filters[i]);
+                    Console.WriteLine(new string('-', Console.BufferWidth - 1));
+                }
+                else
+                {
+                    Console.WriteLine($"{filters[i]->filterId,-6} {filters[i]->filterKey} {filters[i]->action.type,-30} {filters[i]->displayData.name} ");
+                }
+            }
+            PInvoke.FwpmFreeMemory0((void**)&filters);
+            PInvoke.FwpmFilterDestroyEnumHandle0(engineHandle, enumHandle);
+        }
+
+        static unsafe void PrintFilterVerbose(FWPM_FILTER0* filter)
+        {
+            Console.WriteLine($"ID:               {filter->filterId}");
+            Console.WriteLine($"Key:              {filter->filterKey}");
+            Console.WriteLine($"Name:             {filter->displayData.name}");
+            Console.WriteLine($"Description:      {filter->displayData.description}");
+            Console.WriteLine($"Action:           {filter->action.type.ToString().Substring(11)}");
+            if (filter->action.type == FWP_ACTION_TYPE.FWP_ACTION_CALLOUT_TERMINATING
+                || filter->action.type == FWP_ACTION_TYPE.FWP_ACTION_CALLOUT_UNKNOWN
+                || filter->action.type == FWP_ACTION_TYPE.FWP_ACTION_CALLOUT_INSPECTION)
+            {
+                Console.WriteLine($"Callout:          {Helper.TranslateCalloutGuid(filter->action.Anonymous.calloutKey)}");
+            }
+            Console.WriteLine($"Layer:            {Helper.TranslateLayerGuid(filter->layerKey)}");
+            Console.WriteLine($"SubLayer:         {Helper.TranslateSubLayerGuid(filter->subLayerKey)}");
+            Console.WriteLine($"Effective Weight: {Helper.TranslateValue(filter->effectiveWeight)}");
+            if (filter->numFilterConditions > 0)
+            {
+                Console.WriteLine("Conditions:");
+                for (uint j = 0; j < filter->numFilterConditions; j++)
+                {
+                    Console.WriteLine($"  {Helper.TranslateConditionGuid(filter->filterCondition[j].fieldKey),-30} {filter->filterCondition[j].matchType.ToString().Substring(10),-22} {Helper.TranslateValue(filter->filterCondition[j].conditionValue)}");
+                }
+            }
+        }
+
+        static unsafe void QueryFilterById(HANDLE engineHandle, ulong id)
+        {
+            FWPM_FILTER0* filter = null;
+            uint dwRet = PInvoke.FwpmFilterGetById0(engineHandle, id, &filter);
+            if (dwRet != 0)
+            {
+                Console.WriteLine($"FwpmFilterGetById0 failed with error code: 0x{dwRet:X8}");
+                return;
+            }
+
+            PrintFilterVerbose(filter);
+        }
+
+        static void RemoveFilterById(HANDLE engineHandle, ulong id)
+        {
+            var dwRet = PInvoke.FwpmFilterDeleteById0(engineHandle, id);
+            if (dwRet != 0)
+            {
+                Console.WriteLine($"FwpmFilterDeleteById0 failed with error code: 0x{dwRet:X8}");
+            }
+            else
+            {
+                Console.WriteLine($"Filter with ID {id} removed successfully");
+            }
+        }
+
+        static unsafe void AddFilter(HANDLE engineHandle, string name, string description, string layer, string action, IEnumerable<(string, string, string)> conditions, ulong weight)
+        {
+            ulong filterId;
+            var actionType = action.ToLower() switch
+            {
+                "block" => FWP_ACTION_TYPE.FWP_ACTION_BLOCK,
+                "permit" => FWP_ACTION_TYPE.FWP_ACTION_PERMIT,
+                _ => throw new NotSupportedException("Action not supported")
+            };
+            fixed (char* pName = name)
+            {
+                fixed (char* pDescription = description)
+                {
+                    var filter = new FWPM_FILTER0
+                    {
+                        displayData = new FWPM_DISPLAY_DATA0
+                        {
+                            name = pName,
+                            description = pDescription
+                        },
+                        layerKey = Helper.GetLayerGuid(layer),
+                        action = new FWPM_ACTION0
+                        {
+                            type = actionType,
+                        },
+                        weight = new FWP_VALUE0
+                        {
+                            type = FWP_DATA_TYPE.FWP_UINT64,
+                            Anonymous = new FWP_VALUE0._Anonymous_e__Union
+                            {
+                                uint64 = &weight
+                            }
+                        },
+                    };
+                    // TODO: Parse conditions...
+                    var dwRet = PInvoke.FwpmFilterAdd0(engineHandle, &filter, (PSECURITY_DESCRIPTOR)null, &filterId);
+                    if (dwRet != 0)
+                    {
+                        Console.WriteLine($"FwpmFilterAdd0 failed with error code: 0x{dwRet:X8}");
+                    }
+                }
+            }
+            Console.WriteLine($"Filter added successfully");
+            Console.WriteLine();
+            QueryFilterById(engineHandle, filterId);
+        }
+    }
+}
